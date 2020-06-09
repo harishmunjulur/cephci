126dc2f6ac445d8dc354a571cef91586ace19e0b
harishmunjulur <hmunjulu@redhat.com>
2020-06-09 05:06:53 +0000

    Using local git branch util_changes
    Signed-off-by: harishmunjulur <hmunjulu@redhat.com>

--- tests/rbd_system.py
+++ tests/rbd_system.py
@@ -1,24 +1,24 @@
-import logging
-
-log = logging.getLogger(__name__)
-
+import logging as log
 
 def run(**kw):
     log.info("Running rbd tests")
     ceph_nodes = kw.get('ceph_nodes')
+    log.info("{hary}".format(hary=kw))
+    log.info(ceph_nodes)
     rgw_client_nodes = []
     for node in ceph_nodes:
         if node.role == 'client':
             rgw_client_nodes.append(node)
-    git_url = 'https://github.com/red-hat-storage/ceph-qe-scripts.git'
-    git_clone = 'git clone ' + git_url
+    git_url = 'https://github.com/harishmunjulur/ceph-qe-scripts/'
+    git_clone = 'git clone --branch util_change ' + git_url
     client_node = rgw_client_nodes[0]
     # cleanup any existing stale test dir
     test_folder = 'rbd-tests'
-    client_node.exec_command(cmd='rm -rf ' + test_folder)
+    client_node.exec_command(cmd='sudo rm -rf ' + test_folder)
     client_node.exec_command(cmd='mkdir ' + test_folder)
     client_node.exec_command(cmd='cd ' + test_folder + ' ; ' + git_clone)
     client_node.exec_command(cmd='sudo pip install boto names PyYaml ConfigParser')
+    client_node.exec_command(cmd='sudo yum -y install python3')
     config = kw.get('config')
     script_name = config.get('test_name')
     timeout = config.get('timeout', 1800)
@@ -26,7 +26,7 @@ def run(**kw):
         ec_pool_arg = ' --ec-pool-k-m ' + config.get('ec-pool-k-m')
     else:
         ec_pool_arg = ''
-    command = 'sudo python ~/' + test_folder + '/ceph-qe-scripts/rbd/system/' + script_name + ec_pool_arg
+    command = 'sudo python3 ~/' + test_folder + '/ceph-qe-scripts/rbd/system/' + script_name + ec_pool_arg
     stdout, stderr = client_node.exec_command(cmd=command, timeout=timeout, check_ec=False)
     output = stdout.read().decode()
     if output:
